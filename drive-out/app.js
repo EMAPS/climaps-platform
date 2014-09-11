537506eba9ef7a5fec8ca0f9b14268b27291491b
Daniele Guido <gui.daniele@gmail.com>
2014-09-11 18:48:31 +0200

    drive-out added

---
+++ drive-out/app.js
@@ -0,0 +1,165 @@
+'use strict';
+
+var drivein  = require('./drivein'),
+    colors   = require('cli-color'),
+    settings = require('./settings'),
+    
+    Promise  = require('promise'),
+
+    fs = require('fs');
+
+
+console.log(colors.bold('driving out'));
+
+
+function fwrite(path, contents) {
+  return new Promise(function (resolve, reject) {
+    fs.open(path, 'w', function(err, fd){
+      if(err)
+        return reject(err);
+
+      console.log(colors.white('writing file'), colors.blink(path));
+      fs.write(fd, contents);
+      fs.close(fd, function(){
+        return resolve();
+      })
+    });
+  });
+};
+
+
+
+/*
+  crating / updating contents folder
+
+*/
+fs.existsSync('contents') || fs.mkdirSync('contents');
+fs.existsSync('contents/narratives') || fs.mkdirSync('contents/narratives');
+fs.existsSync('contents/maps') || fs.mkdirSync('contents/maps');
+
+/*
+  Map request
+*/
+drivein.getFolderContents(settings.folders.maps).then(function (folders) {
+  //console.log(folders);
+  var maps = folders.filter(function(d){ return d.slug.indexOf('map-') != -1; });
+  
+    
+  for(var i =0; i<maps.length; i++) { // foreach folder found,
+    var files = [];
+    console.log('...', colors.greenBright(maps[i].type), maps[i].slug);
+    if(maps[i].type != "folder") {
+      console.log(colors.yellow('... skipping'), maps[i].type.yellow, maps[i].slug, ': not a folder');
+      continue;
+    };
+    files = drivein.getFolderContentsSync(maps[i].id);
+    // get map title from inslide the document
+    for(var j =0; j<files.length; j++) {
+      console.log('      ', colors.greenBright(files[j].type), files[j].slug, files[j].type == "PNG Image",files[j].slug.indexOf("cover"));
+      
+      files[j].sections = [];
+
+      if(files[j].type == "Document") {
+        var html = drivein.getHtmlSync(files[j].id),
+            body = html.match(/<body[^>]*>(.*?)<\/body>/i)[1],
+            title,
+            subtitle;
+
+        if (files[j].slug.indexOf('-description')!= -1) { // narrative specific approach
+          try{
+            title = body.match(/class="[^"]*title"[^>]*>(.*?)<\/p>/i)[1].replace(/<[^>]*>/g, "");
+            subtitle = body.match(/class="[^"]*subtitle"[^>]*>(.*?)<\/p>/i)[1].replace(/<[^>]*>/g, "");
+          } catch(e) {
+            console.log(e);
+          }
+          maps[i].title = title || maps[i].title;
+          maps[i].subtitle = subtitle || '';
+          
+          files[j].title = title || files[j].title;
+          files[j].subtitle = subtitle || '';
+
+          files[j].sections.push(body);
+
+        } else if (files[j].type == "Document" && files[j].slug.indexOf('-metadata')!= -1) { 
+          files[j].sections.push(body); // spplit in section? other stugffs?
+          //console.log('metadata', body.split(/<h1(.*?)>/))
+        }
+
+      } else if (files[j].type == "PNG Image" && files[j].slug.indexOf('cover')!= -1) { 
+        console.log('daiosdiaodiaoidoasidosaidoidoaids', files[j])
+        maps[i].cover = files[j].src; // spplit in section? other stugffs?
+          //console.log('metadata', body.split(/<h1(.*?)>/))
+      };
+    }
+    fwrite('contents/maps/'+ maps[i].slug +'.json', JSON.stringify(files));
+    //break; // firs 
+      //console.log('writing contents of', folders[i].type.yellow, folders[i].slug);
+  }
+  fwrite('contents/maps.json', JSON.stringify(maps));
+});
+
+
+/*
+  start narratives
+*/
+drivein.getFolderContents(settings.folders.narratives).then(function (folders) {
+  
+  //console.log(folders);
+  var narratives = folders.filter(function(d){ return d.slug.indexOf('narrative') != -1; });
+  
+    
+  for(var i =0; i<narratives.length; i++) { // foreach folder found,
+    var files = [];
+    console.log('...', colors.greenBright(narratives[i].type), narratives[i].slug);
+    if(narratives[i].type != "folder") {
+      console.log(colors.yellow('... skipping'), narratives[i].type.yellow, narratives[i].slug, ': not a folder');
+      continue;
+    };
+    files = drivein.getFolderContentsSync(narratives[i].id);
+    // get map title from inslide the document
+    for(var j =0; j<files.length; j++) {
+      console.log('      ', colors.greenBright(files[j].type), files[j].slug, files[j].slug.indexOf('narrative-'));
+      
+      files[j].sections = [];
+
+      if(files[j].type == "Document") {
+        var html = drivein.getHtmlSync(files[j].id),
+            body = html.match(/<body[^>]*>(.*?)<\/body>/i)[1],
+            title,
+            subtitle;
+
+        if (files[j].slug.indexOf('narrative-')!= -1) { // narrative specific approach
+          //console.log(body);
+          try{
+            title = body.match(/class="[^"]*title"[^>]*>(.*?)<\/p>/i)[1].replace(/<[^>]*>/g, "");
+            subtitle = body.match(/class="[^"]*subtitle"[^>]*>(.*?)<\/p>/i)[1].replace(/<[^>]*>/g, "");
+          } catch(e) {
+            console.log(e);
+          }
+          narratives[i].title = title || narratives[i].title;
+          narratives[i].subtitle = subtitle || '';
+          
+          files[j].title = title || files[j].title;
+          files[j].subtitle = subtitle || '';
+          // split narratives into sections based on h1s, extract the links and so on
+
+          files[j].sections.push(body);
+
+        } else if (files[j].type == "Document" && files[j].slug.indexOf('-metadata')!= -1) { 
+          files[j].sections.push(body); // spplit in section? other stugffs?
+          //console.log('metadata', body.split(/<h1(.*?)>/))
+        }
+
+      } else if (files[j].type == "PNG Image" && files[j].slug.indexOf('cover')!= -1) { 
+        narratives[i].cover = files[j].src; // spplit in section? other stugffs?
+          //console.log('metadata', body.split(/<h1(.*?)>/))
+      };
+    }
+    fwrite('contents/narratives/'+ narratives[i].slug +'.json', JSON.stringify(files));
+    //break; // firs 
+    // break
+      //console.log('writing contents of', folders[i].type.yellow, folders[i].slug);
+  }
+  console.log('hey', narratives)
+  fwrite('contents/narratives.json', JSON.stringify(narratives));
+});
---
+++ drive-out/drivein.js
@@ -0,0 +1,219 @@
+'use strict';
+var settings = require('./settings'),
+    request  = require('request'),
+    requestSync  = require('request-sync'),
+    colors   = require('cli-color'),
+    Promise  = require('promise');
+
+
+
+/*
+  This silly function return the callback(object) of the object having the key=value.
+  This is a specific function for scraping the query results retrieved by YQL
+*/
+var findAs = function(obj, key, value, callback) { // return the very first object, search is a couple key value
+  for(var i in obj.div){
+    if(typeof value === 'function' && obj.div[i][key])// looking only for the key args 
+      return callback(obj.div[i]);
+
+    if(i == key && obj.div[i] == value)
+      return callback(obj.div);
+
+    if(obj.div[i][key] && obj.div[i][key] == value){
+      return callback(obj.div[i]);
+    }
+
+    if(typeof obj.div[i] === 'object'){
+      var loop = findAs(obj.div[i], key, value, callback);
+      if(loop !== undefined)
+        return loop;
+    }
+  }
+  return undefined; // i.e. not found
+};
+
+
+
+
+/*
+  Return clean slug from a string
+*/
+var slugify = function(text) {
+  return text.toString().toLowerCase()
+    .replace(/[\s_]+/g, '-')           // Replace spaces and underscore with -
+    .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+    .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+    .replace(/^-+/, '')             // Trim - from start of text
+    .replace(/-+$/, '');            // Trim - from end of text
+};
+
+
+
+/*
+This is a specific function for obtain a structured json object from the json query results retrieved by YQL
+*/
+var struct = function(item) {
+  var title = findAs(item, 'class', 'flip-entry-title', function(d){
+        return {
+          text: d.p.replace(/\([^\)]*\)/g, '').split(/^\d+\s/).pop(), // vcleaned text
+          original: d.p // original text
+        };
+      }),
+
+      type = findAs(item, 'class', 'flip-entry-thumb', function(d){
+        return d.img.alt; // et oui monsieur
+      }) || 'folder',
+
+      id = item.id.substring(6), // usually google public pages shows id like 'entry-0ByZTyEnzm9qqTFlOazlfa3BSSEE'
+
+      result = { 
+        title: title.text,
+        slug: slugify(title.original),
+        sort: title.original,
+        id: id,
+        type: type
+      }; // uh?
+  
+  if(result.type === 'folder') {
+    return result;
+  };
+  
+  result.src = findAs(item, 'class', 'flip-entry-thumb', function(d){
+    return d.img.src.split(/=s\d+$/).shift();
+  });
+  
+  return result;
+};
+
+
+
+/*
+  Exemple: 
+*/
+exports.getHtml = function(fileId, format) {
+  var f = format || 'html'; // format can either be html or txt
+  
+  console.log(colors.yellow('getting file contents'), 'of', fileId);
+  
+  return new Promise(function (resolve, reject) {
+    request({
+      url:'https://docs.google.com/feeds/download/documents/export/Export?id='+fileId+'&exportFormat=html'
+    }, function (err, res, body) {
+      if (err) {
+          return reject(err);
+      } else if (res.statusCode !== 200) {
+          err = new Error("Unexpected status code: " + res.statusCode);
+          err.res = res;
+          return reject(err);
+      }
+      // do other magic stuff with text ...
+      resolve(body);
+    });
+  });
+};
+
+
+
+exports.getHtmlSync = function(fileId, format) {
+  var f = format || 'html'; // format can either be html or txt
+  var response = requestSync({
+    url:'https://docs.google.com/feeds/download/documents/export/Export?id='+fileId+'&exportFormat=' + f,
+    method: 'GET'
+  });
+  console.log('         ',colors.cyan('get html sync'), 'of', fileId);
+  return response.body;
+}
+
+
+exports.getFolderContentsSync = function(folderId) {
+  console.log("      ",colors.cyan("get folder contents sync"), 'of', folderId);
+  
+  var contents = [],
+      response = requestSync({
+        url: settings.urls.yql,
+        qs: {
+          q: 'select * from html where url="' + settings.urls.folderview + folderId + '" and xpath=\'//div[@class="flip-entry"]\'',
+          format:'json',
+          diagnostics: true
+        },
+        method: 'GET' 
+      });
+
+  console.log("      ", '... statusCode', colors.green(response.statusCode));
+  //console.log(response.body);
+
+  try{
+    var body = JSON.parse(response.body);
+  } catch (e) {
+    console.error("Parsing error:", e);
+  }
+  
+  if(!body.query) {
+    console.log("Unexpected body content");
+  };
+
+  if(!body.query.results){
+    throw "Unexpected results content";
+  };
+  
+  if(body.query.results.div.length) {
+    for(var i in body.query.results.div) {
+      contents.push(struct(body.query.results.div[i]));
+    };
+  } else { // just one item under folder
+    contents.push(struct(body.query.results.div));
+  }
+  console.log('      ',contents.length, colors.white("files/folders found"));
+  return contents;
+}
+
+
+
+exports.getFolderContents = function(folderId) {
+  console.log(colors.cyan('get folder contents'), 'of', folderId);
+  return new Promise(function (resolve, reject) {
+    var contents = [];
+
+    request({
+      url: settings.urls.yql,
+       json:true,
+      qs: {
+        q: 'select * from html where url="' + settings.urls.folderview + folderId + '" and xpath=\'//div[@class="flip-entry"]\'',
+            format:'json',
+            diagnostics: true
+          }, 
+      }, function (err, res, body) {
+        
+        if (err) {
+            return reject(err);
+        } else if (res.statusCode !== 200) {
+            err = new Error("Unexpected status code: " + res.statusCode);
+            err.res = res;
+            return reject(err);
+        }
+        
+        if(!body.query) {
+          err = new Error("Unexpected body content: " + body);
+          err.res = res;
+          return reject(err);
+        };
+
+        if(!body.query.results){
+          err = new Error("Unexpected results content: " + body);
+          err.res = res;
+          return reject(err);
+        }
+
+        console.log('... statusCode', colors.green(res.statusCode));
+  
+        if(body.query.results.div.length) {
+          for(var i in body.query.results.div) {
+            contents.push(struct(body.query.results.div[i]));
+          };
+        } else { // just one item under folder
+          contents.push(struct(body.query.results.div));
+        }
+        return resolve(contents);
+    });
+  });
+};
