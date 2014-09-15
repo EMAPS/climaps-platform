ea0be7d9adf5f65d21b9e39e9bfbac6030c34557
Daniele Guido <gui.daniele@gmail.com>
2014-09-15 09:03:20 +0200

    drive-out export improved

--- drive-out/app.js
+++ drive-out/app.js
@@ -42,7 +42,74 @@ process.on('uncaughtException', function (exception) {
 fs.existsSync('../contents') || fs.mkdirSync('../contents');
 fs.existsSync('../contents/narratives') || fs.mkdirSync('../contents/narratives');
 fs.existsSync('../contents/maps') || fs.mkdirSync('../contents/maps');
+fs.existsSync('../contents/pages') || fs.mkdirSync('../contents/pages');
 
+drivein.getFolderContents(settings.folders.pages).then(function (pages) {
+
+  for(var i =0; i<pages.length; i++) { 
+    var contents = [];
+    
+    console.log('   ', colors.greenBright(pages[i].type), pages[i].slug);
+    
+    if(pages[i].type != "Document") {
+      console.log(colors.yellow('... skipping'), pages[i].type.yellow, pages[i].slug, ': not a gdoc file');
+      continue;
+    };
+    
+    var html = drivein.getHtmlSync(pages[i].id),
+        body,
+        sections,
+        title,
+        subtitle,
+        page = {
+          sections:[]
+        };
+
+    try{
+      body = html.match(/<body[^>]*>(.*?)<\/body>/i)[1]
+    } catch(e) {
+      console.log(colors.red('warning'), e)
+      continue;
+    }
+
+    try{
+      page.title = body.match(/class="[^"]*title"[^>]*>(.*?)<\/p>/i)[1].replace(/<[^>]*>/g, "");
+    } catch(e) {
+      console.log(colors.red('warning *page* title'), e)
+      
+    }
+    sections = body.split(/<h2/); // search for h2 tags...
+    //if(sections.length<2)
+    //  continue;
+    //console.log(sections);
+
+    for(var j=0; j<sections.length; j++) {
+      if(!sections[j].length)
+        continue;
+      // get the title
+      try{
+        title = sections[j].match(/<h1[^>]*>(.*?)<\/h1>/i)[1].replace(/<[^>]*>/g, "");
+      } catch(e) {
+        //console.log(colors.red('warning, *section title* not found'), e, sections[j]);
+        continue;
+      };
+      // get the subtitle
+     
+      page.sections.push({
+        html: '<h2' + sections[j],
+        title: title
+      });
+
+
+    }
+
+    fwrite('../contents/pages/'+ pages[i].slug +'.json', JSON.stringify(page, null,2));
+    
+    
+  }
+
+});
+return;
 /*
   Map request
 */
