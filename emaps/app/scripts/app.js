33537842bae5754d23973aeeaee93ba8fe0fa8f9
gianni <scnepa222@gmail.com>
2014-09-16 15:19:14 +0200

    bootstrap app structure

---
+++ emaps/app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name emapsApp
+ * @description
+ * # emapsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('emapsApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
