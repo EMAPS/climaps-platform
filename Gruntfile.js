29c78524ca5acc65343ca414419da5188a10f78a
gianni <scnepa222@gmail.com>
2014-11-03 10:53:48 +0100

    grunt build updated

--- Gruntfile.js
+++ Gruntfile.js
@@ -332,6 +332,18 @@ module.exports = function (grunt) {
           cwd: 'bower_components/bootstrap/dist',
           src: 'fonts/*',
           dest: '<%= yeoman.dist %>'
+        },
+        {
+          expand: true,
+          cwd: '<%= yeoman.app %>',
+          src: ['styles/fonts/*'],
+          dest: '<%= yeoman.dist %>'
+        },
+        {
+          expand: true,
+          cwd: '<%= yeoman.app %>',
+          src: ['contents/{,*/}*'],
+          dest: '<%= yeoman.dist %>'
         }]
       },
       styles: {
