cc9c3444e76c7d1d33bb852d833e899e2040b7bd
gianni <scnepa222@gmail.com>
2014-11-04 14:02:06 +0100

    added config sample file

---
+++ config.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name emapsApp.config
+ * @description
+ * # config
+ * Constant in the emapsApp.
+ */
+angular.module('emapsApp')
+  .constant('DISQUS_SHORTNAME', 'your_disqus_shortname')
+  .constant('GA_ID', 'XX-XXXXXXX-XX')
